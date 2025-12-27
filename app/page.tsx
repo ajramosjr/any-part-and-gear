15443a34ee6c55006a7843439dd0f275a33adbe7
ajramosjr <any-part@outlook.com>
2025-12-27 07:58:18 -0500

    Fix import path case sensitivity for Header component

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     </main>
   );
 }
-import Header from "./components/Header";
+import Header from "./Components/Header";
 
 export default function Home() {
   return (
