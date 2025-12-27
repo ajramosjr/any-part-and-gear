424862e34780eb90f23b0b47d85c81ce81779e5b
ajramosjr <any-part@outlook.com>
2025-12-27 17:11:52 -0500

    Fix Header import casing and page export

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Header from "./components/Header";
+import header from "./components/header";
 export default function Home() {
   return (
     <>
-      <Header />
+      <header />
 
       <main style={{ padding: "40px", textAlign: "center" }}>
         <h1>Any-Part & Gear</h1>
