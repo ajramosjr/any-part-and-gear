604740d97647aa7e91c6f2789c3f233a2bac438e
ajramosjr <any-part@outlook.com>
2025-12-28 06:34:39 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <header />
+      <main style={{ padding: "40px" }}>
+        <h1>Welcome to Any-Part & Gear</h1>
+        <p>
+          Buy, sell, or trade car, truck, and boat parts — all in one place.
+        </p>
+      </main>
+    </>
+  );
+}
