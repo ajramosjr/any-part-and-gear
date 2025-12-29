27fb7b0bd8cf94d20072e8a9ab013cc613c659a5
ajramosjr <any-part@outlook.com>
2025-12-29 15:24:39 -0500

    Update and rename page.tsx to page.tsx

---
+++ app/Listings/page.tsx
@@ -0,0 +1,8 @@
+export default function ListingsPage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Listings Page Works</h1>
+      <p>If you see this, routing is correct.</p>
+    </main>
+  );
+}
