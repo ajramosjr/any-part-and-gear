84ca482d097cf9bcf2f060f3f3d07ee8259ffb5c
ajramosjr <any-part@outlook.com>
2025-12-31 13:53:27 -0500

    Update page.tsx

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,16 +1,10 @@
-export const dynamic = "force-dynamic";
-
-import { Suspense } from "react";
 import SellClient from "./SellClient";
 
 export default function SellPage() {
   return (
     <main style={{ padding: "40px" }}>
       <h1>Sell a Part</h1>
-
-      <Suspense fallback={null}>
-        <SellClient />
-      </Suspense>
+      <SellClient />
     </main>
   );
 }
