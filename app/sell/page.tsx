a5a0eb49e7761fb3657ec6b310dff27549a52538
ajramosjr <any-part@outlook.com>
2025-12-31 14:22:28 -0500

    Update page.tsx

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,10 +1,7 @@
-import SellClient from "./SellClient";
-
 export default function SellPage() {
   return (
-    <main style={{ padding: "40px" }}>
-      <h1>Sell a Part</h1>
-      <SellClient />
-    </main>
+    <div style={{ color: "red", fontSize: "40px" }}>
+      🚨 SELL PAGE TEST 🚨
+    </div>
   );
 }
