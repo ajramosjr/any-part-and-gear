b1f0e930937bb4abc1b034b33cad74197a667a44
ajramosjr <any-part@outlook.com>
2025-12-31 15:34:16 -0500

    DEBUG: confirm sell page rendering

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,10 +1,15 @@
-import SellClient from "./SellClient";
-
 export default function SellPage() {
   return (
-    <main style={{ padding: "40px", maxWidth: "600px" }}>
-      <h1>Sell a Part</h1>
-      <SellClient />
-    </main>
+    <div
+      style={{
+        background: "red",
+        color: "white",
+        padding: "40px",
+        fontSize: "24px",
+        fontWeight: "bold",
+      }}
+    >
+      🚨 PAGE.TSX IS RENDERING 🚨
+    </div>
   );
 }
