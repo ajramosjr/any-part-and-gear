a3694f1d0c0e0e26340e94cd2d485db7b7f640e0
ajramosjr <any-part@outlook.com>
2025-12-31 03:51:20 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,17 @@
-"use client";
-
-import { Toaster } from "react-hot-toast";
+import "./globals.css";
+import ToasterClient from "./ui/toaster-client";
 
 export default function RootLayout({
   children,
-  }: {
-    children: React.ReactNode;
-    }) {
-      return (
-          <html lang="en">
-                <body>
-                        <Toaster position="top-right" />
-                                {children}
-                                      </body>
-                                          </html>
-                                            );
-                                            }
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ToasterClient />
+        {children}
+      </body>
+    </html>
+  );
+}
