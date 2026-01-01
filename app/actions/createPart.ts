5cd73e3ccaa80bc13f554f2082411597482e3e99
ajramosjr <any-part@outlook.com>
2026-01-01 15:13:52 -0500

    Update createPart.ts

--- app/actions/createPart.ts
+++ app/actions/createPart.ts
@@ -12,6 +12,26 @@ export async function createPart(formData: FormData) {
     process.env.SUPABASE_SERVICE_ROLE_KEY!
   );
 
+  const image = formData.get("image") as File;
+
+let image_url = null;
+
+if (image && image.size > 0) {
+  const fileName = `${Date.now()}-${image.name}`;
+
+  const { data, error } = await supabase.storage
+    .from("part-images")
+    .upload(fileName, image);
+
+  if (error) {
+    redirect("/sell?error=Image upload failed");
+  }
+
+  image_url = supabase.storage
+    .from("part-images")
+    .getPublicUrl(fileName).data.publicUrl;
+}
+  
   const { error } = await supabase.from("parts").insert({
     title,
     description,
