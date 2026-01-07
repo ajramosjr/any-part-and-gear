b60e47948db49809a681e2347dff6c7522e9bb69
ajramosjr <any-part@outlook.com>
2026-01-07 12:33:40 -0500

    Update supabaseServer.ts

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,14 +1,16 @@
-import { createClient } from "@supabase/supabase-js";
 import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
 
 export function createSupabaseServerClient() {
-  return createClient(
+  const cookieStore = cookies();
+
+  return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
-          return cookies().get(name)?.value;
+          return cookieStore.get(name)?.value;
         },
       },
     }
