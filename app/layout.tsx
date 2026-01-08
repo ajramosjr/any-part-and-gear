23997c71168d45e9e7ff15e4c652516786e8d9a4
ajramosjr <any-part@outlook.com>
2026-01-07 20:03:17 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,22 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <ToasterClient />
+
+        {/* Global Navigation */}
+        <nav style={{ padding: 16, borderBottom: "1px solid #444" }}>
+          <Link href="/browse" style={{ marginRight: 16 }}>
+            Browse
+          </Link>
+          <Link href="/sell" style={{ marginRight: 16 }}>
+            Sell
+          </Link>
+          <Link href="/my-listings">
+            My Listings
+          </Link>
+        </nav>
+
         {children}
       </body>
     </html>
   );
 }
-<Link href="/my-listings">My Listings</Link>
