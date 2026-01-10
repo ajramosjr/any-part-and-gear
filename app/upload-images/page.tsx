748925aea1c333636b865327b2cd0d272a267605
ajramosjr <any-part@outlook.com>
2026-01-10 09:21:56 -0500

    Update page.tsx

--- app/upload-images/page.tsx
+++ app/upload-images/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { createClient } from "@/utils/supabase/client";
+import { createClient } from "@/lib/supabaseClient";
 
 export default function UploadImagesPage() {
   const supabase = createClient();
