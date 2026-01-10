fde0c3c85c573767a1feb0aab7c4fa17ecbfb51e
ajramosjr <any-part@outlook.com>
2026-01-10 15:58:05 -0500

    Update page.tsx

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -1,4 +1,4 @@
-import { createClient } from "@/lib/supabase";
+import { createClient } from "@/lib/supabaseClient";
 
 export default async function BrowsePage() {
   const supabase = await createClient();
