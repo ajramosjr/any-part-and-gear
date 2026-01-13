9326ddd9cb72f68b9014c78db6f8269a27b3f040
ajramosjr <any-part@outlook.com>
2026-01-12 19:40:51 -0500

    Update page.tsx

--- app/upload-images/page.tsx
+++ app/upload-images/page.tsx
@@ -10,18 +10,71 @@ export default function UploadImagesPage() {
     bodyType: "",
   });
 
+  const [images, setImages] = useState<File[]>([]);
+
+  function handleImageChange(e: React.ChangeEvent<HTMLInputElement>) {
+    if (!e.target.files) return;
+    setImages(Array.from(e.target.files));
+  }
+
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Upload Images</h1>
+    <div style={{ padding: "2rem", maxWidth: 500 }}>
+      <h1>Upload Vehicle Images</h1>
 
       <input
         type="text"
-        placeholder="Vehicle model"
-        value={vehicle.model || ""}
+        placeholder="Make"
+        value={vehicle.make}
+        onChange={(e) =>
+          setVehicle({ ...vehicle, make: e.target.value })
+        }
+      />
+
+      <br /><br />
+
+      <input
+        type="text"
+        placeholder="Model"
+        value={vehicle.model}
         onChange={(e) =>
           setVehicle({ ...vehicle, model: e.target.value })
         }
       />
+
+      <br /><br />
+
+      <input
+        type="text"
+        placeholder="Year"
+        value={vehicle.year}
+        onChange={(e) =>
+          setVehicle({ ...vehicle, year: e.target.value })
+        }
+      />
+
+      <br /><br />
+
+      <input
+        type="text"
+        placeholder="Body Type"
+        value={vehicle.bodyType}
+        onChange={(e) =>
+          setVehicle({ ...vehicle, bodyType: e.target.value })
+        }
+      />
+
+      <br /><br />
+
+      <input
+        type="file"
+        multiple
+        accept="image/*"
+        onChange={handleImageChange}
+      />
+
+      <p style={{ marginTop: 10 }}>
+        {images.length} image(s) selected
+      </p>
     </div>
   );
 }
