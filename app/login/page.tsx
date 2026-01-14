c2b7a2a0e6bee93cde94f6a75e4d5f6c25e4562d
ajramosjr <any-part@outlook.com>
2026-01-13 20:54:15 -0500

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { useState } from "react";
+import { supabase } from "@/lib/supabaseClient";
 
-
-export default function LoginPage()
-
+export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
