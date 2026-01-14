53da6bc7f3b1979f646741e29f24e38e75ca9849
ajramosjr <any-part@outlook.com>
2026-01-14 06:04:47 -0500

    Update supabaseBrowser.ts

--- lib/supabaseBrowser.ts
+++ lib/supabaseBrowser.ts
@@ -1,9 +1,6 @@
-// lib/supabaseBrowser.ts
 import { createBrowserClient } from "@supabase/ssr";
 
-export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
-}
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
