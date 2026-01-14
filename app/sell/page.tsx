cef5d0e5928a82650732891d63ba4b9b06c9bfb0
ajramosjr <any-part@outlook.com>
2026-01-13 20:50:37 -0500

    Update page.tsx

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,5 +1,14 @@
+import { supabase } from "@/lib/supabaseClient";
 import SellClient from "./SellClient";
 
-export default function SellPage() {
+export default async function SellPage() {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return <p>Please log in to sell items.</p>;
+  }
+
   return <SellClient />;
 }
