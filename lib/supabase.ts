93b6e6cf6c3a9ab366cc32586e68e5b58fd55de8
ajramosjr <any-part@outlook.com>
2026-01-15 03:36:05 -0500

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { supabase } from "@/lib/supabaseClient";
 
-export const supabase = createClient(
+export (
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
