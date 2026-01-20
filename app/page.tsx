48f7a6b2d78e34d0c769943ed1a5073a34e5def6
ajramosjr <any-part@outlook.com>
2026-01-20 16:18:47 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
           src="/logo.png"
           alt="Any-Part & Gear Logo"
           width={240}
-          height={200}
+          height={100}
           priority
           className="hero-logo"
         />
