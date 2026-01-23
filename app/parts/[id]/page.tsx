9c6d065a777aa702e608809ef3584e4dcfb4b71b
ajramosjr <any-part@outlook.com>
2026-01-23 16:11:12 -0500

    Update page.tsx

--- app/parts/[id]/page.tsx
+++ app/parts/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation";
-import supabase from "@/lib/supabaseServer";
+import { supabase } from "@/lib/supabaseServer";
 import TradeRequestForm from "@/components/TradeRequestForm";
 
 interface PartPageProps {
