35933c503e1f8f9c7f87a576e1eb740fb00179d8
ajramosjr <any-part@outlook.com>
2026-01-29 20:20:14 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 
-const nextConfig: NextConfig = {
-  experimental: {
-    // serverActions removed
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
