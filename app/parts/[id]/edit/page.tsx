6096f92ed1874c813f3f0595464f851d0301c03c
ajramosjr <any-part@outlook.com>
2026-02-02 19:09:23 -0500

    Update page.tsx

--- app/parts/[id]/edit/page.tsx
+++ app/parts/[id]/edit/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
-import { createClient } from "@/lib/supabaseServer";
+import { createClient } from "@/lib/supabase/server";
 
 interface EditPartPageProps {
   params: {
