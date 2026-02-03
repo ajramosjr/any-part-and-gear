35d3b14c4085f4ff1d570d4622bdc9cf9953f950
ajramosjr <any-part@outlook.com>
2026-02-02 20:09:39 -0500

    Update page.tsx

--- app/parts/page.tsx
+++ app/parts/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { supabase } from "@/lib/supabaseClient.ts";
+import { supabase } from "@/lib/supabaseClient";
 
 type Part = {
   id: number; // ✅ MUST be number
