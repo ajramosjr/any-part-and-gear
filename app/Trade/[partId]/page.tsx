02c33a52d0e80f4bfd5e41c8b916fc720bb772c1
ajramosjr <any-part@outlook.com>
2026-02-03 16:06:18 -0500

    Update page.tsx

--- app/Trade/[partId]/page.tsx
+++ app/Trade/[partId]/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 import Image from "next/image";
-import { createClient } from "@/lib/supabaseClient";
+import { supabase } from "@/lib/supabaseClient";
 
 type Part = {
   id: string;
