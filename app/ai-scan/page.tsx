fa4b30f1c1a6ee616c5fa0ff498ab359534d6bf2
ajramosjr <any-part@outlook.com>
2026-02-03 20:37:40 -0500

    Update page.tsx

--- app/ai-scan/page.tsx
+++ app/ai-scan/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { createClient } from "@/lib/supabaseClient";
+import supabase from "@/lib/supabaseClient";
 
 export default function AiScanPage() {
   const supabase = createClient();
