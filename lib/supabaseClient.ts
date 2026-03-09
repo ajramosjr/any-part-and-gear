59a66364ced868ce89b6dcda7a57a45f8ce35d07
ajramosjr <any-part@outlook.com>
2026-03-09 01:20:25 -0400

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,18 +1,8 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-<<<<<<< HEAD
 export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 }
-=======
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = 
-  supabaseUrl,
-  supabaseAnonKey
-);
->>>>>>> 8b64255 (import { supabase } from "@/lib/supabaseClient";)
