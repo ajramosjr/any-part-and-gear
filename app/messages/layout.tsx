34a97ab1c252d100cffcf251a3b009544f89824a
ajramosjr <any-part@outlook.com>
2026-03-09 07:27:30 -0400

    Update layout.tsx

--- app/messages/layout.tsx
+++ app/messages/layout.tsx
@@ -1,11 +1,16 @@
 import { redirect } from "next/navigation";
-import { supabase } from "@/lib/supabaseClient";
+import { createClient } from "@supabase/supabase-js";
 
 export default async function MessagesLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+
   const {
     data: { user },
   } = await supabase.auth.getUser();
