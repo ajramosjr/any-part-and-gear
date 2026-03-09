b95ad511d55f8915f4716a0f27d4f40c79327dff
ajramosjr <any-part@outlook.com>
2026-03-09 09:38:27 -0400

    Update useUnreadMessages.ts

--- hooks/useUnreadMessages.ts
+++ hooks/useUnreadMessages.ts
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-import { supabase } from "@/lib/supabaseClient";
+import { createClient } from "@/lib/supabaseClient";
+
+const supabase = createClient();
 
 export function useUnreadMessages() {
   const [count, setCount] = useState(0);
