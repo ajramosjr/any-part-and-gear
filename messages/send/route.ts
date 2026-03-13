21d3ee66126fb3b5f8255aba7766ce2e3e9cb2b3
ajramosjr <any-part@outlook.com>
2026-03-13 15:06:03 -0400

    Update route.ts

--- messages/send/route.ts
+++ messages/send/route.ts
@@ -2,15 +2,18 @@ import { supabase } from "@/lib/supabaseClient";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  const supabase = await );
   const formData = await req.formData();
 
-  await supabase.from("messages").insert({
-    sender_email: "buyer@guest.com", // later: auth user
+  const { error } = await supabase.from("messages").insert({
+    sender_email: "buyer@guest.com",
     receiver_email: formData.get("receiver_email"),
     part_title: formData.get("part_title"),
-    message: formData.get("message"),
+    content: formData.get("message"),
   });
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 
   return NextResponse.redirect(new URL("/messages", req.url));
 }
