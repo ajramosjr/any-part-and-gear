31a5b0d1cd2b929e8d01a8adf203efa650c2b0a0
ajramosjr <any-part@outlook.com>
2026-03-14 15:23:37 -0400

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,34 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
 
-<Link href="/" className="flex items-center">
-  <Image
-    src="/logo.png"
-    alt="Any Part & Gear"
-    width={200}
-    height={90}
-    className="h-12 w-auto"
-    priority
-  />
-</Link>
+export default function NavBar() {
+  return (
+    <header className="border-b bg-white">
+      <div className="max-w-6xl mx-auto flex items-center justify-between p-4">
+
+        {/* Logo */}
+        <Link href="/" className="flex items-center">
+          <Image
+            src="/logo.png"
+            alt="Any Part & Gear"
+            width={200}
+            height={80}
+            className="h-12 w-auto"
+            priority
+          />
+        </Link>
+
         {/* Navigation */}
         <nav className="flex gap-6 text-gray-700 font-medium items-center">
-
           <Link href="/browse">Browse</Link>
-
           <Link href="/sell">Sell</Link>
-
           <Link href="/my-listings">My Listings</Link>
-
-          {/* Messages */}
           <Link href="/messages">Messages</Link>
-
           <Link href="/login">Login</Link>
-
         </nav>
 
       </div>
-
     </header>
   );
 }
