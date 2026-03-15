27d25b7ccb00c152ea81d364a3d6ceade0ef3171
ajramosjr <any-part@outlook.com>
2026-03-15 16:53:20 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,9 @@ export default function Home() {
       <div className="text-center mb-10">
 
         <h1 className="text-4xl font-bold text-blue-900">
-          Find Rare Parts & Trusted Sellers
+          Find rare parts, local deals, and trusted sellers.
+
+The smarter marketplace for car, boat, and gear enthusiasts.
         </h1>
 
         <p className="text-gray-500 mt-2">
